08e9bd3b92f344f8c71d83a7459e8d59c04e2895
Michal Kolínek <michal.kolinek@gmail.com>
2017-04-08 17:39:49 +0000

    Project structure change, added exec scripts

---
+++ exec/saveToDb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?php
+
+$dns = 'mysql:host=localhost;dbname=SmartHome;charset=utf8';
+$connection = new PDO($dns, 'root', '0i549bmK');
+
+$params = explode(',', $argv[1]);
+
+$nodeId = $params[0];
+$voltage = $params[1];
+$temperature = $params[2];
+$humidity = $params[3];
+$date = date('Y-m-d h:i:s');
+
+$sql = 'INSERT INTO templog(node, date, temperature, humidity, voltage) 
+	VALUES('.$nodeId.', "'.$date.'", '.$temperature.', '.$humidity.', '.$voltage.')';
+
+$connection->query($sql);
+
+echo 'OK';
+exit;
