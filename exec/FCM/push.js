d89921402a101733550f71fa0a08b13e7b357c75
Michal Kolinek <michal.kolinek@gmail.com>
2019-01-05 20:16:30 +0100

    added pressure sensor, read all local sensors at once

--- exec/FCM/push.js
+++ exec/FCM/push.js
@@ -6,7 +6,8 @@ const fcm = new FCM(serverKey);
 const devices = [
     'c_PfoKvw0xM:APA91bH48R0MTbtKUd_6ObDOnh8C70tiANYvpSsdWvG11hIAQGz-iX1c0E2GcYirpsOYwRsbTpYJJ2H9EYgR9dl70uJmmXoL9Km_srvWdpQbKNkp3CFhoQxkH-ggrrqUhEhIQ66_RIIZ',
     'fm6oxv3Dg-g:APA91bFA8I5pdwXw4wD7pjZm6OJOQuIDDrdllyApvriDPhKP9OTm8S6SMC-ptwtrB-WvKWePUSorkUUYr5UKptd-cBeMVPMRdvSPLz-iZRhOCwSIJt-2-ANpD001cTTCijwzxiZLkUWc',        
-    'fhIGLhQHG68:APA91bGmQc60csKDbKDxQ5xcBS1DS4Cqs02PHsWsPph6U1ss70F-9Fk7JYXwbTCxsZhDIolIblB2lY_X4G9CdHazuvGVJL3k-Zg8iEGaCyC-iKSS3XhrIadwroS87SrtL6m-yszVx8td'
+    'fhIGLhQHG68:APA91bGmQc60csKDbKDxQ5xcBS1DS4Cqs02PHsWsPph6U1ss70F-9Fk7JYXwbTCxsZhDIolIblB2lY_X4G9CdHazuvGVJL3k-Zg8iEGaCyC-iKSS3XhrIadwroS87SrtL6m-yszVx8td',
+    'dhxgsl1yCRY:APA91bEt1B-u9INLruoJxgb052-zaTgjVW3G5ORGO-s5XrKTWBvsfFDAgVR6i8jmbvsD2PEpFWdGR1AF8aOp4d12uJeCcmuZIX1UFAleR1teOJ_z5JMxoHy6P5HgQjsiRUY4QzA0fpQ3EZ0EPC1ilUjJTtXIibredw'
 ];
 
 devices.forEach((to) => {
--- exec/saveToDb.sh
+++ exec/saveToDb.sh
@@ -3,9 +3,6 @@
 
 include('db.php');
 
-//$dns = 'mysql:host=localhost;dbname=SmartHome;charset=utf8';
-//$connection = new PDO($dns, 'root', '0i549bmK');
-
 $params = explode(',', $argv[1]);
 
 $nodeId = $params[0];
@@ -14,10 +11,11 @@ $temperature = $params[2] ? $params[2] : 'NULL';
 $humidity = $params[3] ? $params[3] : 'NULL';
 $moisture = $params[4] ? $params[4] : 'NULL';
 $output = $params[5] || $params[5] === '0' ? $params[5] : 'NULL';
+$pressure = $params[6] || $params[6] === '0' ? $params[6] : 'NULL';
 $date = date('Y-m-d H:i:s');
 
-$sql = 'INSERT INTO templog(node, date, temperature, humidity, moisture, output, voltage)
-	VALUES('.$nodeId.', "'.$date.'", '.$temperature.', '.$humidity.', '.$moisture.', '.$output.', '.$voltage.')';
+$sql = 'INSERT INTO templog(node, date, temperature, humidity, moisture, pressure, output, voltage)
+    VALUES('.$nodeId.', "'.$date.'", '.$temperature.', '.$humidity.', '.$moisture.', '.$pressure.', '.$output.', '.$voltage.')';
 $result = $connection->query($sql);
 
 if($result) {
